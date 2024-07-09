2f862812ac7b7800858277e627a60d72a336e210
Yassine94110 <yassineouhmmou@gmail.com>
2024-07-09 11:44:36 +0200

    update dex

--- foundry/src/ILiquidityPool.sol
+++ foundry/src/ILiquidityPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.13;
 
 interface ILiquidityPool {
     function addInitialLiquidity(uint256 _assetOneAmount, uint256 _assetTwoAmount) external;
--- foundry/src/LiquidityPool.sol
+++ foundry/src/LiquidityPool.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
